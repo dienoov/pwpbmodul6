692ca15281db491c99b81bc468d66790e3cf6e84
dienoov <alvinodien@yahoo.co.id>
2021-02-04 17:23:08 +0700

    Fix active state nav

--- app/src/main/java/com/tol/pwpbmodul6/Mandiri.java
+++ app/src/main/java/com/tol/pwpbmodul6/Mandiri.java
@@ -1,11 +1,15 @@
 package com.tol.pwpbmodul6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,35 +20,42 @@ public class Mandiri extends AppCompatActivity {
     @BindView(R.id.frame_kita)
     FrameLayout frameKita;
 
+    @BindView(R.id.nav_kita)
+    BottomNavigationView navKita;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mandiri);
         ButterKnife.bind(this);
 
+        navKita.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager director = getSupportFragmentManager();
+                FragmentTransaction rekening = director.beginTransaction();
+
+                switch (item.getItemId()) {
+
+                    case R.id.ke_rumah:
+                        rekening.replace(R.id.frame_kita, new HomeKita());
+                        rekening.commit();
+                        return true;
+
+                    case R.id.profil_kita:
+                        rekening.replace(R.id.frame_kita, new ProfileKita());
+                        rekening.commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
         FragmentManager director = getSupportFragmentManager();
         FragmentTransaction rekening = director.beginTransaction();
 
         rekening.add(R.id.frame_kita, new ProfileKita());
         rekening.add(R.id.frame_kita, new HomeKita());
-        rekening.commit();
-    }
-
-    @OnClick(R.id.ke_rumah)
-    public void keRumah() {
-        FragmentManager director = getSupportFragmentManager();
-        FragmentTransaction rekening = director.beginTransaction();
-
-        rekening.replace(R.id.frame_kita, new HomeKita());
-        rekening.commit();
-    }
-
-    @OnClick(R.id.profil_kita)
-    public void keProfil() {
-        FragmentManager director = getSupportFragmentManager();
-        FragmentTransaction rekening = director.beginTransaction();
-
-        rekening.replace(R.id.frame_kita, new ProfileKita());
         rekening.commit();
     }
 }
