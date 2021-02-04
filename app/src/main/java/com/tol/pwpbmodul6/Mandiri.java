3e612eb539ee3332d55cbc20b69f7d3d835c3ea6
dienoov <alvinodien@yahoo.co.id>
2021-02-04 17:07:04 +0700

    (+) Profil jalur mandiri

--- app/src/main/java/com/tol/pwpbmodul6/Mandiri.java
+++ app/src/main/java/com/tol/pwpbmodul6/Mandiri.java
@@ -9,6 +9,7 @@ import android.widget.FrameLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Mandiri extends AppCompatActivity {
 
@@ -24,7 +25,26 @@ public class Mandiri extends AppCompatActivity {
         FragmentManager director = getSupportFragmentManager();
         FragmentTransaction rekening = director.beginTransaction();
 
+        rekening.add(R.id.frame_kita, new ProfileKita());
         rekening.add(R.id.frame_kita, new HomeKita());
+        rekening.commit();
+    }
+
+    @OnClick(R.id.ke_rumah)
+    public void keRumah() {
+        FragmentManager director = getSupportFragmentManager();
+        FragmentTransaction rekening = director.beginTransaction();
+
+        rekening.replace(R.id.frame_kita, new HomeKita());
+        rekening.commit();
+    }
+
+    @OnClick(R.id.profil_kita)
+    public void keProfil() {
+        FragmentManager director = getSupportFragmentManager();
+        FragmentTransaction rekening = director.beginTransaction();
+
+        rekening.replace(R.id.frame_kita, new ProfileKita());
         rekening.commit();
     }
 }
